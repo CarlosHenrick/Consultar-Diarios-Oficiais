bb394d96d36621a06819f07cb40b6b13633e930f
Carlos Henrique <54175838+CarlosHenrick@users.noreply.github.com>
2025-08-20 12:59:16 -0300

    Proxy

---
+++ api/dosp-proxy.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+    // URL real do DOSP
+    const targetUrl = "https://diariooficial.prefeitura.sp.gov.br/md_epubli_controlador.php?acao=materias_pesquisar";
+
+    // repassa a requisição para o site alvo
+    const r = await fetch(targetUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: req.body
+    });
+
+    const text = await r.text();
+
+    // adiciona os cabeçalhos de CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    // retorna o HTML do DOSP para o navegador
+    res.status(200).send(text);
+}
